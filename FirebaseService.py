fdb62d36f19ea79669983986257cfe1010985656
Vishnu Sundaresan <68603272+juliussneezer04@users.noreply.github.com>
2023-10-22 05:55:12 -0400

    add firebase write chat method

--- FirebaseService.py
+++ FirebaseService.py
@@ -1,6 +1,11 @@
+from typing import TypedDict, Union
 from firebase import firebase
 from dotenv import load_dotenv
+from datetime import datetime
 import os
+import uuid
+
+import pytz
 
 # Load environment variables
 load_dotenv()
@@ -13,6 +18,15 @@ if not FIREBASE_DATABASE_URL:
 # Initialize Firebase
 firebase_app = firebase.FirebaseApplication(FIREBASE_DATABASE_URL, None)
 
+class FirebaseMessage(TypedDict):
+  isSentByBot: bool
+  text: str
+  sentAt: str # ISO 8601
+  id: Union[str, int]
+
+def email_to_key(email: str):
+    return email.replace('.', '_')
+
 def save_token(user_id, token):
     result = firebase_app.put(f'/userTokens/{user_id}', 'token', token)
     return result
@@ -20,3 +34,14 @@ def save_token(user_id, token):
 def get_token(user_id):
     result = firebase_app.get(f'/userTokens/{user_id}', 'token')
     return result or {}
+
+def write_chat_message(user_email: str, message: str):
+    id = str(uuid.uuid4())
+    message: FirebaseMessage = {
+        "id": id,
+        "text": message,
+        "isSentByBot": True,
+        "sentAt": datetime.now(tz=pytz.timezone("America/New_York")).isoformat(),
+    }
+    result = firebase_app.post(f"/chats/{user_email}", message)
+    return result is not None
