3d2651e94e3b9e10ee61b7f9389ceaffbdc48d2f
Vishnu Sundaresan <68603272+juliussneezer04@users.noreply.github.com>
2023-10-21 23:49:18 -0400

    fix warnings

--- constants.py
+++ constants.py
@@ -12,9 +12,9 @@ class Command(str, Enum):
 
 
 BASE_URL = (
-    "https://avon-seven.vercel.app"
-    if os.getenv("ENVIRONMENT") != "dev"
-    else "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000"
+    if os.getenv("ENVIRONMENT") != "prod"
+    else "https://avon-seven.vercel.app"
 )
 GOOGLE_SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
 
