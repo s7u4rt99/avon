fa4959e7c4f4e02c51287fd1cc7405e6fadeb917
Joshua Nee <joshua@intuneauto.com>
2023-10-22 06:40:38 -0400

    format

--- db.py
+++ db.py
@@ -123,7 +123,19 @@ def add_task(user_id: int, name: str, description: str = "", recurring: bool = F
     )
 
 
-def edit_task(task_id: int, name: str, description: str = "", deadline: datetime = None, estimated_duration: int = None, frequency: int = None, state: str = None, event_start: datetime = None, event_end: datetime = None, adjusted_start: datetime = None, adjusted_end: datetime = None):
+def edit_task(
+    task_id: int,
+    name: str,
+    description: str = "",
+    deadline: datetime = None,
+    estimated_duration: int = None,
+    frequency: int = None,
+    state: str = None,
+    event_start: datetime = None,
+    event_end: datetime = None,
+    adjusted_start: datetime = None,
+    adjusted_end: datetime = None,
+):
     data = {}
 
     if name != "":
@@ -143,10 +155,10 @@ def edit_task(task_id: int, name: str, description: str = "", deadline: datetime
 
     if state != None:
         data["state"] = state
-    
+
     if event_start != None:
         data["event_start"] = event_start
-    
+
     if event_end != None:
         data["event_end"] = event_end
 
@@ -177,7 +189,11 @@ def fetch_tasks_formatted(user_id: int):
 
 # for testing purposes only
 def message(user_id: int, message: str):
-    return (supabase.table("MessageTest").insert({"user_id": user_id, "message": message}).execute())
+    return (
+        supabase.table("MessageTest")
+        .insert({"user_id": user_id, "message": message})
+        .execute()
+    )
 
 
 def reply_flow(todo_id: int, message: str):
@@ -207,15 +223,22 @@ def reply_flow(todo_id: int, message: str):
             edit_task(task_id=todo_id, frequency=int(message))
 
     if todo_state == "adjusting":
-        if (message.lower() == "ok"):
+        if message.lower() == "ok":
             # add to calender here
             refresh_token = get_user(todo["user_id"])["google_refresh_token"]
-            add_calendar_event(todo, refresh_token, todo["name"], todo["description"], todo["adjusted_start"], todo["adjusted_end"])
+            add_calendar_event(
+                todo,
+                refresh_token,
+                todo["name"],
+                todo["description"],
+                todo["adjusted_start"],
+                todo["adjusted_end"],
+            )
             edit_task(task_id=todo.id, state="added_to_calendar")
         else:
             # please dont come here
             print("wrong input")
-            
+
     update_state(todo)
 
 
@@ -225,52 +248,55 @@ def update_state(todo):
             if todo.deadline == None:
                 edit_task(task_id=todo.id, state="missing_deadline")
                 message = f"When is {todo.name} due?"
-                write_chat_message(get_user(todo["user_id"])[
-                                   "email"], message, f"/reply/{todo.id}")
+                write_chat_message(
+                    get_user(todo["user_id"])["email"], message, f"/reply/{todo.id}"
+                )
                 return
             elif todo.estimated_duration == None:
                 edit_task(task_id=todo.id, state="missing_duration")
                 message = f"What is the estimated duration for {todo.name}?"
-                write_chat_message(get_user(todo["user_id"])[
-                                   "email"], message, f"/reply/{todo.id}")
+                write_chat_message(
+                    get_user(todo["user_id"])["email"], message, f"/reply/{todo.id}"
+                )
                 return
 
         elif todo.type == "events":
             if todo.event_start == None:
                 edit_task(task_id=todo.id, state="missing_event_start")
                 message = "When does the event start?"
-                write_chat_message(get_user(todo["user_id"])[
-                                   "email"], message, f"/reply/{todo.id}")
+                write_chat_message(
+                    get_user(todo["user_id"])["email"], message, f"/reply/{todo.id}"
+                )
                 return
             elif todo.event_end == None:
                 edit_task(task_id=todo.id, state="missing_event_end")
                 message = "When does the event end?"
-                write_chat_message(get_user(todo["user_id"])[
-                                   "email"], message, f"/reply/{todo.id}")
+                write_chat_message(
+                    get_user(todo["user_id"])["email"], message, f"/reply/{todo.id}"
+                )
                 return
 
         elif todo.type == "habits":
             if todo.estimated_duration == None:
                 edit_task(task_id=todo.id, state="missing_duration")
                 message = f"How long do you estimate it takes to do {todo.name}?"
-                write_chat_message(get_user(todo["user_id"])[
-                                   "email"], message, f"/reply/{todo.id}")
+                write_chat_message(
+                    get_user(todo["user_id"])["email"], message, f"/reply/{todo.id}"
+                )
                 return
             elif todo.frequency == None:
                 edit_task(task_id=todo.id, state="missing_frequency")
                 message = f"How often would you like to do {todo.name}?"
-                write_chat_message(get_user(todo["user_id"])[
-                                   "email"], message, f"/reply/{todo.id}")
+                write_chat_message(
+                    get_user(todo["user_id"])["email"], message, f"/reply/{todo.id}"
+                )
                 return
-
 
         edit_task(task_id=todo.id, state="adjusting")
         # call the ai here
         refresh_token = get_user(todo["user_id"])["google_refresh_token"]
         time_now = datetime.now(tz=pytz.timezone("America/New_York"))
-        midnight = datetime(
-            time_now.year, time_now.month, time_now.day, 23, 59, 59
-        )
+        midnight = datetime(time_now.year, time_now.month, time_now.day, 23, 59, 59)
         events = get_calendar_events(
             refresh_token=refresh_token,
             timeMin=time_now.isoformat() + "Z",
@@ -280,9 +306,9 @@ def update_state(todo):
 
         formatted_events = [
             {
-                "start_time": datetime.fromisoformat(event["start"]["dateTime"]).strftime(
-                    "%I:%M%p"
-                ),
+                "start_time": datetime.fromisoformat(
+                    event["start"]["dateTime"]
+                ).strftime("%I:%M%p"),
                 "end_time": datetime.fromisoformat(
                     event.get("end")["dateTime"]
                     if not event.get("endTimeUnspecified", False)
@@ -293,7 +319,6 @@ def update_state(todo):
             }
             for event in events
         ]
-
 
         todo = {}
         if todo.type == "tasks":
@@ -320,7 +345,7 @@ def update_state(todo):
                 "frequency": todo.frequency,
                 "estimated_duration": todo.estimated_duration,
             }
-        
+
         pre_message = f"""
         {todo}
 
@@ -328,10 +353,15 @@ def update_state(todo):
         {formatted_events}
         """
         message = adjusting_existing_schedule_prompt(pre_message)
-        write_chat_message(get_user(todo["user_id"])[
-            "email"], message, f"/reply/{todo.id}")
+        write_chat_message(
+            get_user(todo["user_id"])["email"], message, f"/reply/{todo.id}"
+        )
         resParsed: dict = loads(message)
-        edit_task(task_id=todo.id, adjusted_start=resParsed["start_time"], adjusted_end=resParsed["end_time"])
+        edit_task(
+            task_id=todo.id,
+            adjusted_start=resParsed["start_time"],
+            adjusted_end=resParsed["end_time"],
+        )
 
 
 def add_new_task_flow(message: str):
@@ -346,22 +376,65 @@ def add_new_task_flow(message: str):
     # if gpt returns "none", convert to none
 
     resParsed: dict = loads(todo_data)
-    
+
     name = resParsed["description"]
 
     if resParsed["type"] == "Task":
         type = "tasks"
         deadline = None if resParsed["deadline"] == "none" else resParsed["deadline"]
-        estimated_duration = None if resParsed["estimated_duration"] == "none" else resParsed["estimated_duration"]
-        todo = supabase.table("Tasks").insert({"type":type, "name": name, "deadline" : deadline, "estimated_duration": estimated_duration}).execute()
+        estimated_duration = (
+            None
+            if resParsed["estimated_duration"] == "none"
+            else resParsed["estimated_duration"]
+        )
+        todo = (
+            supabase.table("Tasks")
+            .insert(
+                {
+                    "type": type,
+                    "name": name,
+                    "deadline": deadline,
+                    "estimated_duration": estimated_duration,
+                }
+            )
+            .execute()
+        )
     elif resParsed["type"] == "Event":
         type = "events"
-        event_start = None if resParsed["start_time"] == "none" else resParsed["start_time"]
+        event_start = (
+            None if resParsed["start_time"] == "none" else resParsed["start_time"]
+        )
         event_end = None if resParsed["end_time"] == "none" else resParsed["end_time"]
-        todo = supabase.table("Tasks").insert({"type":type, "name": name, "event_start" : event_start, "event_end": event_end}).execute()
+        todo = (
+            supabase.table("Tasks")
+            .insert(
+                {
+                    "type": type,
+                    "name": name,
+                    "event_start": event_start,
+                    "event_end": event_end,
+                }
+            )
+            .execute()
+        )
     elif resParsed["type"] == "Habit":
         type = "habits"
         frequency = None if resParsed["frequency"] == "none" else resParsed["frequency"]
-        estimated_duration = None if resParsed["estimated_duration"] == "none" else resParsed["estimated_duration"]
-        todo = supabase.table("Tasks").insert({"type":type, "name": name, "frequency" : frequency, "estimated_duration": estimated_duration}).execute()
+        estimated_duration = (
+            None
+            if resParsed["estimated_duration"] == "none"
+            else resParsed["estimated_duration"]
+        )
+        todo = (
+            supabase.table("Tasks")
+            .insert(
+                {
+                    "type": type,
+                    "name": name,
+                    "frequency": frequency,
+                    "estimated_duration": estimated_duration,
+                }
+            )
+            .execute()
+        )
     update_state(todo)
